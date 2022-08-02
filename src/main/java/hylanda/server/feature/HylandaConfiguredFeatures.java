044e7fc5ded2cc22c56e6f53c29d621a19b85f48
Willatendo <williamjswindler@gmail.com>
2022-08-02 00:33:59 -0600

    Update HylandaConfiguredFeatures.java

--- src/main/java/hylanda/server/feature/HylandaConfiguredFeatures.java
+++ src/main/java/hylanda/server/feature/HylandaConfiguredFeatures.java
@@ -3,6 +3,7 @@ package hylanda.server.feature;
 import hylanda.HylandaMod;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -10,5 +11,5 @@ import net.minecraftforge.registries.RegistryObject;
 public class HylandaConfiguredFeatures {
 	public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES = DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, HylandaMod.ID);
 
-	public static final RegistryObject<ConfiguredFeature<?, ?>> BIOQUOIA_TREE = CONFIGURED_FEATURES.register("bioquoia_tree", () -> new ConfiguredFeature<>(new BioquoiaTreeFeature(NoneFeatureConfiguration.CODEC), new NoneFeatureConfiguration()));
+	public static final RegistryObject<ConfiguredFeature<?, ?>> BIOQUOIA_TREE = CONFIGURED_FEATURES.register("bioquoia_tree", () -> new ConfiguredFeature<>(new BioquoiaTreeFeature(NoneFeatureConfiguration.CODEC), FeatureConfiguration.NONE));
 }
